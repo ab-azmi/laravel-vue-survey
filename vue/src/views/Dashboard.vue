c298b9c26c7825d7a9dbde81e0105e8e5c451254
ab-azmi <azmi1752@students.unnes.ac.id>
2022-12-08 15:56:25 +0700

    Setup Vue frontend

---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <PageComponent title="Dashboard">
+    <h1>this is Dashboard</h1>
+  </PageComponent>
+</template>
+
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
+</script>
+
---
+++ vue/src/views/Survey.vue
@@ -0,0 +1,10 @@
+<template>
+  <PageComponent title="Surveys">
+    <h1>this is surveys</h1>
+  </PageComponent>
+</template>
+
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
+</script>
+
